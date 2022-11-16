17ce594349e012d7447ae80397886cea24d9f136
Samsou1 <sami.linard.pro@gmail.com>
2022-11-16 14:16:09 +0100

    implement beforeEach

--- spec/itemsSpec/conjuredItemSpec.js
+++ spec/itemsSpec/conjuredItemSpec.js
@@ -1,9 +1,15 @@
 import ConjuredItem from '../../src/items/conjuredItem.js'
 
 describe("ConjuredItem class", () => {
+  let Q;
+  let S;
+
+  beforeEach(() => {
+    Q = Math.floor(Math.random() * 50);
+    S = Math.floor(Math.random() * 10);
+  });
+
     it("should create an item", () => {
-      const Q = Math.floor(Math.random() * 50);
-      const S = Math.floor(Math.random() * 10);
       const item = new ConjuredItem("Pants", S, Q);
       expect(item.name).toBe("Conjured Pants");
       expect(item.sellIn).toBe(S);
@@ -11,16 +17,13 @@ describe("ConjuredItem class", () => {
     });
 
     it("should lower the quality twice as fast", () => {
-      const Q = Math.floor(Math.random() * 50);
-      const S = Math.floor(Math.random() * 10);
       const item = new ConjuredItem("Pants", S, Q);
       item.updateQuality();
       expect(item.quality).toBe(Q - 2 >= 0 ? Q - 2 : 0);
     });
 
     it("should lower the quality twice as fast while the the expiration date is reached", () => {
-      const Q = Math.floor(Math.random() * 50);
-      const S = -Math.floor(Math.random() * 10);
+      S = - S - 1
       const item = new ConjuredItem("Pants", S, Q);
       item.updateQuality();
       expect(item.quality).toBe(Q - 4 >= 0 ? Q - 4 : 0);
--- spec/itemsSpec/decreasingQualityItemSpec.js
+++ spec/itemsSpec/decreasingQualityItemSpec.js
@@ -1,9 +1,15 @@
 import DecreasingQualityItem from '../../src/items/decreasingQualityItem.js'
 
 describe("DecreasingQualityItem class", () => {
+  let Q;
+  let S;
+
+  beforeEach(() => {
+    Q = Math.floor(Math.random() * 50);
+    S = Math.floor(Math.random() * 10);
+  });
+
   it("should create an item", () => {
-    const Q = Math.floor(Math.random() * 50);
-    const S = Math.floor(Math.random() * 10);
     const item = new DecreasingQualityItem("Pants", S, Q);
     expect(item.name).toBe("Pants");
     expect(item.sellIn).toBe(S);
@@ -11,48 +17,39 @@ describe("DecreasingQualityItem class", () => {
   });
 
   it("should lower the quality", () => {
-    const Q = Math.floor(Math.random() * 50 + 1);
-    const S = Math.floor(Math.random() * 10);
     const item = new DecreasingQualityItem("Pants", S, Q);
     item.updateQuality();
-    expect(item.quality).toBe(Q === 0 ? 0 : Q - 1);
+    expect(item.quality).toBe(Q - 1 >= 0 ? Q - 1 : 0);
   });
 
   it("should lower the sellIn", () => {
-    const Q = Math.floor(Math.random() * 50);
-    const S = Math.floor(Math.random() * 10);
     const item = new DecreasingQualityItem("Pants", S, Q);
     item.updateQuality();
     expect(item.sellIn).toBe(S - 1);
   });
 
   it("should lower the quality twice as fast when we reach the expirable date", () => {
-    const Q = Math.floor(Math.random() * 50);
-    const S = -Math.floor(Math.random() * 10);
+    S = - S -1
     const item = new DecreasingQualityItem("Pants", S, Q);
     item.updateQuality();
-    expect(item.quality).toBe(Q <= 1 ? 0 : Q - 2);
+    expect(item.quality).toBe(Q <= 2 ? 0 : Q - 2);
   });
 
   it("shouldn't create an item with quality over 50", () => {
-    const S = Math.floor(Math.random() * 10);
-    const Q = Math.floor(Math.random() * 50) + 50;
+    Q += 51;
     expect(() => {new DecreasingQualityItem("Pants", S, Q)}).toThrow(new Error("You need to set the quality between 0 and 50")) ;
   });
 
   it("shouldn't create an item with quality over 50", () => {
-    const S = Math.floor(Math.random() * 10);
-    const Q = - Math.floor(Math.random() * 50) - 1;
+    Q = - Q - 1;
     expect(() => {new DecreasingQualityItem("Pants", S, Q)}).toThrow(new Error("You need to set the quality between 0 and 50")) ;
   });
 
   it("shouldn't create an item with no quality", () => {
-    const S = Math.floor(Math.random() * 10);
     expect(() => {new DecreasingQualityItem("Pants", S, undefined)}).toThrow(new Error("You need to set the quality")) ;
   });
 
   it("shouldn't create an item with no sellIn", () => {
-    const Q = Math.floor(Math.random() * 50);
     expect(() => {new DecreasingQualityItem("Pants", undefined, Q)}).toThrow(new Error("You need to set the expiration date")) ;
   });
 
